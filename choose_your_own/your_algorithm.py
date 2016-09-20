373adf1e64257eeb687b6d2cf7ff54ed594d721c
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-09-19 22:25:41 -0400

    Finishing mini project to test various algorithms

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -28,15 +29,35 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 
+from sklearn.metrics import accuracy_score
 
+classifier = {
+  'adaboost': AdaBoostClassifier(n_estimators=100,learning_rate=1,algorithm="SAMME"),
+  'kneigh': KNeighborsClassifier(n_neighbors=3),
+  'randForest': RandomForestClassifier(n_estimators=15,criterion='entropy'),
+  'svc': SVC(kernel="rbf", C=1),
+  'naiveBayes': GaussianNB(),
+  'dtree': DecisionTreeClassifier(min_samples_split=20)
+}
 
+for name, clf in classifier.iteritems():
+  print name
+  t0 = time()
+  clf.fit(features_train, labels_train)
+  print "\tTime to train: ", time() - t0, "s"
 
-
-
-
+  pred = clf.predict(features_test)
+  acc = accuracy_score(labels_test, pred)
+  print "\tPrediction accuracy: ", acc
 
 try:
     prettyPicture(clf, features_test, labels_test)
