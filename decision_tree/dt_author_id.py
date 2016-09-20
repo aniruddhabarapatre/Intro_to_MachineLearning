e969bf537e8cef03859f1dc29b9daa1a0f8358ce
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-09-19 20:05:16 -0400

    Changing percentile for Decision Trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,13 +19,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print "Length of features: ", len(features_train[0])
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time: ", round(time()-t0, 3), "s"
 
-#########################################################
-### your code goes here ###
+t1 = time()
+pred = clf.predict(features_test)
+print "Prediction time: ", round(time()-t1, 3), "s"
 
-
-#########################################################
-
-
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
